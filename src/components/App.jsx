d8e867679d9c2e216eff672cc3050c9d90238427
Yuriy Minashkin <y.minashkin@gmail.com>
2023-04-14 21:13:52 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,40 @@
-export const App = () => {
+import { useEffect } from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+// import { refreshUser } from 'redux/auth/authOperations';
+import { RegisterPage } from 'pages/RegisterPage/RegisterPage';
+import { LoginPage } from 'pages/LoginPage/LoginPage';
+import { MainLayout } from 'pages/MainLayout/MainLayout';
+import { AccountPage } from 'pages/AccountPage/AccountPage';
+import { CalendarPage } from 'pages/CalendarPage/CalendarPage';
+import { ChoosedDay } from 'components/ChoosedDay/ChoosedDay';
+import { ChoosedMonth } from 'components/ChoosedMonth/ChoosedMonth';
+
+const App = () => {
+  const dispatch = useDispatch();
+
+  // useEffect(() => {
+  //   dispatch(refreshUser());
+  // }, [dispatch]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/" element={<MainLayout />}>
+        <Route path="/account" element={<AccountPage />} />
+        <Route path="/calendar" element={<CalendarPage />}>
+          <Route path="month/:currentDate" element={<ChoosedMonth />} />
+          <Route path="day/:currentDay" element={<ChoosedDay />} />
+        </Route>
+      </Route>
+      <Route path="*" element={<Navigate to="/calendar" />} />
+    </Routes>
   );
 };
+
+export { App };
+
+        // <Route path="/login" element={<PublicRoute component={<LoginPage />} />}/>
+        // <Route path="/register" element={<PublicRoute component={<RegisterPage />} />}/>
+        // <Route path="/" element={<PrivateRoute component={<MainLayout />} />}>
