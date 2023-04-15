9c864f8877ac7198a6e01e5c9767f09de72251c8
SerhiiKutovyi <wagenrex@gmail.com>
2023-04-16 02:39:50 +0300

    Update UserForm.jsx

--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -1,5 +1,14 @@
-import { Formik, Form } from 'formik';
+import { Formik, Form, ErrorMessage } from 'formik';
 import { Container, Wrapper, Input } from './UserForm.styled';
+import { object, string, number, date } from 'yup';
+
+const userSchema = object({
+  username: string().max(16).required(),
+  birthday: date().default(() => new Date()),
+  email: string().email().required(),
+  phone: number(),
+  skype: string().max(16),
+});
 
 const initialValues = {
   username: '',
@@ -20,31 +29,40 @@ const UserForm = () => {
       <Wrapper>
         <h1>User Name</h1>
         <p>User</p>
-        <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+        <Formik
+          initialValues={initialValues}
+          onSubmit={handleSubmit}
+          validationSchema={userSchema}
+        >
           <Form autoComplete="off">
             <label htmlFor="username">
               User Name
               <Input type="text" name="username"></Input>
+              <ErrorMessage name="username" />
             </label>
 
             <label htmlFor="birthday">
               Birthday
-              <Input type="text" name="birthday"></Input>
+              <Input type="date" name="birthday"></Input>
+              <ErrorMessage name="birthday" />
             </label>
 
             <label htmlFor="email">
               Email
-              <Input type="text" name="email"></Input>
+              <Input type="email" name="email"></Input>
+              <ErrorMessage name="email" />
             </label>
 
             <label htmlFor="phone">
               Phone
-              <Input type="text" name="phone"></Input>
+              <Input type="tel" name="phone"></Input>
+              <ErrorMessage name="phone" />
             </label>
 
             <label htmlFor="skype">
               Skype
               <Input type="text" name="skype"></Input>
+              <ErrorMessage name="skype" />
             </label>
 
             <button type="submit">Save changes</button>
