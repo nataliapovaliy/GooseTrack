69bc87edb69da83185bad7a73f261d2c901bcf11
xanderkiev2022 <301019a@gmail.com>
2023-04-17 13:10:31 +0300

    more changes

--- src/components/Header/Header.styled.js
+++ src/components/Header/Header.styled.js
@@ -18,3 +18,28 @@ export const H2 = styled.h2`
   text-shadow: 0px 47px 355px rgba(0, 0, 0, 0.07),
     0px 9.4px 57.6875px rgba(0, 0, 0, 0.035);
 `;
+
+export const Motivation = styled.span`
+  margin-right: auto;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 14px;
+  color: var(--title-text-main-color);
+`;
+
+export const InnerWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: auto;
+  position: relative;
+`;
+
+export const Image = styled.img`
+  width: 64px;
+  height: 60px;
+  margin-right: 10px;
+`;
+
+export const Accent = styled.span`
+  color: var(--accent);
+`;
