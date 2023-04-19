a77ed9f10ba4c26b12e2a7bc8134a4267831fb41
MariKa2525 <113701062+MariKa2525@users.noreply.github.com>
2023-04-19 14:37:43 +0300

    add

--- src/components/ChoosedMonth/ChoosedMonth.jsx
+++ src/components/ChoosedMonth/ChoosedMonth.jsx
@@ -4,23 +4,43 @@ import { Monitor } from "./Monitor"
 
 import moment from 'moment'
 import { useState } from "react"
+import { useEffect } from "react"
 
 // window.moment = moment;
 
 export const ChoosedMonth = () => {
     const [displayMode, setDisplayMode] = useState('month')
     const [today, setToday] = useState(moment())
+    const [task, setTask] = useState([])
+
+
+
+
     moment.updateLocale( 'en', {week: {dow: 1}})
+    const URL = 'url'
+
+
+    useEffect(() => {
+        fetch(URL)
+        .then(res => res.json())
+    })
+
+    
    
     const startDay = today.clone().startOf('month').startOf('week');
 
     const prevHandler = () => {
-        // console.log('prev')
         setToday(prev => prev.clone().subtract(1, 'month'))
     }
     const nextHandler = () => {
         setToday(next => next.clone().add(1, 'month'))
     }
+
+
+
+
+
+
     return(
         <>
         <HeaderCalendar today={today} prevHandler={prevHandler} nextHandler={nextHandler} setDisplayMode={setDisplayMode}/>
