8f4490e24bb953a5a532b7efee720cc20c45f242
Maksym-Chmilov <maxchmilov@gmail.com>
2023-04-20 14:16:26 +0300

    login/register done

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,6 +1,4 @@
 import { LoginForm } from 'components/LoginForm/LoginForm';
-// import { Link } from 'react-router-dom';
-// import { LoginFormLayout } from './LoginPage.styled';
 
 export const LoginPage = () => {
   return (
