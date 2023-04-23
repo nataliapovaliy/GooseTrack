3fcd898df2ef70ba27cce8766b9e5a438268170a
xanderkiev2022 <301019a@gmail.com>
2023-04-23 20:43:57 +0300

    minor changes

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,17 +1,25 @@
 import { useSelector } from 'react-redux';
-import { selectUser } from 'redux/auth/auth-selectors';
+import { selectIsLoading, selectUser } from 'redux/auth/auth-selectors';
 import { Name, Avatar, AvatarLetter, AvatarImg } from './UserInfo.styled';
 
 export const UserInfo = () => {
   const selector = useSelector(selectUser);
+  const isLoading = useSelector(selectIsLoading);
+
   const name = selector.user?.name || 'Name';
   const avatar = selector.user?.avatarURL;
   const firstLetter = name.trim().slice(0, 1).toUpperCase();
 
   return (
     <>
-          <Name>{name}</Name>
-          <Avatar>{avatar === null ? (<AvatarLetter>{firstLetter}</AvatarLetter>) : (<AvatarImg src={avatar} alt="Avatar" />)}</Avatar>
+      <Name>{name}</Name>
+      <Avatar>
+        {isLoading
+          ? <AvatarLetter>{firstLetter}</AvatarLetter>
+          : (avatar === null
+            ? (<AvatarLetter>{firstLetter}</AvatarLetter>)
+            : (<AvatarImg src={avatar} alt="Avatar" />))}
+      </Avatar>
     </>
   );
 };
