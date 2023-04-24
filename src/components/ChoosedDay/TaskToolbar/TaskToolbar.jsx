bb5dcfd05b4ee5043aeea6da4de086a61a99cea9
DanilVelichko <velichko.danil@gmail.com>
2023-04-24 21:58:17 +0200

    modal done

--- src/components/ChoosedDay/TaskToolbar/TaskToolbar.jsx
+++ src/components/ChoosedDay/TaskToolbar/TaskToolbar.jsx
@@ -1,20 +1,37 @@
 // import { useState } from 'react';
-import { TaskToolbarStyled, TaskToolbarBtn, Svg } from './TaskToolbar.styled';
+import {
+  TaskToolbarStyled,
+  TaskToolbarBtn,
+  Svg,
+  TaskModalChangeStatusWrapper,
+  TaskModalChangeStatusBtn,
+  StateStatus,
+} from './TaskToolbar.styled';
 
-import iconTrash from '../../../images/icons.svg';
+import icon from '../../../images/icons.svg';
 import { useDispatch } from 'react-redux';
 import {
   openModalUpDateTask,
   openModalConfirmation,
 } from 'redux/modal/globalSlice';
+import { useEffect, useState } from 'react';
+import { fetchTasks, updateTask } from 'redux/tasks/tasks-operations';
 // import { deleteTask } from '../../redux/tasks/tasks-operations';
 // import { toast } from 'react-toastify';
 // import { TaskModal } from '../TaskModal/TaskModal';
 // import { TaskModalContext } from '../../context/TaskModalContext';
 
 export const TaskToolbar = ({ task, getTask }) => {
-  // const [IsModalOpen, setIsModalOpen] = useState(false);
-  // const toggleModal = () => setIsModalOpen(prev => !prev);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  // console.log('isModalOpen :>> ', isModalOpen);
+  const statusStates = ['To do', 'In progress', 'Done'];
+  const [status, setStatus] = useState(task.status);
+
+  const toggleModal = () => {
+    setIsModalOpen(prev => !prev);
+
+    console.log('toggleModal', isModalOpen);
+  };
 
   // const dispatch = useDispatch();
   // console.log('isModalOpen :>> ', isModalOpen);
@@ -39,23 +56,59 @@ export const TaskToolbar = ({ task, getTask }) => {
     getTask(task);
   };
 
+  useEffect(() => {
+    dispatch(fetchTasks())
+  }, [dispatch, status]);
+
+
   return (
     <>
       <TaskToolbarStyled>
-        <TaskToolbarBtn>
+        <TaskToolbarBtn onClick={() => toggleModal()}>
           <Svg>
-            <use xlinkHref={iconTrash + '#icon-round-arrow'}></use>
+            <use xlinkHref={icon + '#icon-round-arrow'}></use>
           </Svg>
+          {isModalOpen && (
+            <TaskModalChangeStatusWrapper data-modal="true">
+              {statusStates
+                .filter(states => states !== status)
+                .map((state, index) => (
+                  <TaskModalChangeStatusBtn
+                    key={index}
+                    onClick={() => {
+                      console.log('task._id :>> ', task._id, state);
+
+                     
+                      const taskForUpdate = {
+                        id: task._id,
+                        task: {
+                          status: state,
+                        },
+                      };
+                      dispatch(updateTask(taskForUpdate, task._id));
+                      console.log('status :>> ', status);
+                       setStatus(state);
+                    }}
+                  >
+                    <StateStatus>{state}</StateStatus>
+                    <Svg>
+                      <use xlinkHref={icon + '#icon-round-arrow'}></use>
+                    </Svg>
+                  </TaskModalChangeStatusBtn>
+                ))}
+            </TaskModalChangeStatusWrapper>
+          )}
         </TaskToolbarBtn>
+
         <TaskToolbarBtn onClick={() => openModal(task._id)}>
           <Svg>
-            <use xlinkHref={iconTrash + '#icon-pencil'}></use>
+            <use xlinkHref={icon + '#icon-pencil'}></use>
           </Svg>
         </TaskToolbarBtn>
 
         <TaskToolbarBtn onClick={confirmationOpen}>
           <Svg>
-            <use xlinkHref={iconTrash + '#icon-trash'}></use>
+            <use xlinkHref={icon + '#icon-trash'}></use>
           </Svg>
         </TaskToolbarBtn>
       </TaskToolbarStyled>
--- src/components/ChoosedDay/TaskToolbar/TaskToolbar.styled.js
+++ src/components/ChoosedDay/TaskToolbar/TaskToolbar.styled.js
@@ -28,3 +28,46 @@ export const Svg = styled.svg`
     stroke: var(--accent);
   }
 `;
+
+export const TaskModalChangeStatusWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  align-content: flex-start;
+  justify-content: center;
+  gap: 10px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background: var(--modal-background-color);
+  border-radius: 5px;
+  /* padding: 20px; */
+  width: 147px;
+  height: 90px;
+  z-index: 100;
+
+`;
+
+export const TaskModalChangeStatusBtn = styled.button`
+  display: flex;
+  align-items: start;
+  justify-content: space-around;
+
+  
+  padding: 0px;
+  margin: 0px;
+  border: none;
+  background: none;
+  cursor: pointer;
+  color: var(--title-text-main-color);
+  &:hover {
+    color: var(--accent);
+  }
+`;
+
+export const StateStatus = styled.div`
+margin: 0px 10px 0px 0px;
+  padding: 0px 0px 0px 0px;
+  
+  `;
