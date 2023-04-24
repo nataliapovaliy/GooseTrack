3e1091ea3ec2a99bc1a86dbafde3032db51f3a13
MariKa2525 <113701062+MariKa2525@users.noreply.github.com>
2023-04-24 16:39:41 +0300

    add

--- src/components/ChoosedMonth/CalendarTable/CalendarTable.js
+++ src/components/ChoosedMonth/CalendarTable/CalendarTable.js
@@ -6,12 +6,12 @@ import {
   GridWrapper,
   RowInCell,
   ShowDayWrapper,
-  TaskItem,
   TaskListWrapper,
 } from './CalendarTable.styled';
 import moment from 'moment';
 import { useSelector } from 'react-redux';
 import { selectArrTasks } from 'redux/tasks/tasks-selectors';
+import { CalendarTaskDay } from './CalendarTaskDay/CalendarTaskDay';
 
 export const CalendarTable = ({ startDay, today, totalDays }) => {
   const day = startDay.clone().subtract(1, 'day');
@@ -22,52 +22,52 @@ export const CalendarTable = ({ startDay, today, totalDays }) => {
   const tasks = useSelector(selectArrTasks);
   // console.log("++++", tasks);
 
-  let priorityColor = '';
-  let priorityTextColor = '';
+  let xxx =[]
 
   return (
     <ContainerCalendar>
-    <GridWrapper>
-      {daysArray.map(dayItem => (
-        <CellWrapper
-          key={dayItem.unix()}
-          isSelectedMonth={isSelectedMonth(dayItem)}
-        >
-          <RowInCell justifyContent={'flex-end'}>
-            <ShowDayWrapper>
-              <DayWrapper>
-                {isCurrentDay(dayItem) ? (
-                  <CurrentDay>{dayItem.format('D')}</CurrentDay>
-                ) : (
-                  dayItem.format('D')
-                )}
-              </DayWrapper>
-            </ShowDayWrapper>
-            <TaskListWrapper>
-              {tasks.map(
-                task =>
-                {
-                  
-                  if (task.priority === 'Low') {
-                    priorityColor = 'var(--priority-low-color)' ;
-                    priorityTextColor = 'var(--task-low-color)';
-                  } else if (task.priority === 'Medium') {
-                    priorityColor = 'var(--priority-med-color)';
-                    priorityTextColor = 'var(--task-med-color)';
-                  } else if (task.priority === 'High') {
-                    priorityColor = 'var(--priority-high-color)' ;
-                    priorityTextColor = 'var(--task-high-color)';
-                  }
-                  return task.createAt === dayItem.format('YYYY-MM-DD') && (
-                    <TaskItem key={task._id} style={{ backgroundColor: priorityColor, color: priorityTextColor }}>{task.title}</TaskItem>
-                  )
-                }
-              )}
-            </TaskListWrapper>
-          </RowInCell>
-        </CellWrapper>
-      ))}
-    </GridWrapper>
-   </ContainerCalendar>
+      <GridWrapper>
+        {daysArray.map(dayItem => (
+          <CellWrapper
+            key={dayItem.unix()}
+            isSelectedMonth={isSelectedMonth(dayItem)}
+          >
+            <RowInCell justifyContent={'flex-end'}>
+              <ShowDayWrapper>
+                <DayWrapper>
+                  {isCurrentDay(dayItem) ? (
+                    <CurrentDay>{dayItem.format('D')}</CurrentDay>
+                  ) : (
+                    dayItem.format('D')
+                  )}
+                </DayWrapper>
+              </ShowDayWrapper>
+              <TaskListWrapper>
+                {tasks.map(task => {
+
+                  return (
+                    task.createAt === dayItem.format('YYYY-MM-DD') &&  (
+                      <li key={task._id}>
+                        <CalendarTaskDay task={task} />
+                      </li>
+                    )
+                  );
+                })}
+              </TaskListWrapper>
+            </RowInCell>
+          </CellWrapper>
+        ))}
+      </GridWrapper>
+    </ContainerCalendar>
   );
 };
+
+
+{
+  // const xxx = tasks.filter(task => 
+  //   task.priority === 'Low' && task.createAt === dayItem.format('YYYY-MM-DD')
+  //   )
+  // console.log("!!!!!", xxx);
+
+
+}
