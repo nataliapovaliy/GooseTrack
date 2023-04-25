45f2e1f9852937cf2c1261f52f955832767da145
xanderkiev2022 <301019a@gmail.com>
2023-04-25 17:35:43 +0300

    overlay click close

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,9 +7,10 @@ import { UserInfo } from 'components/UserInfo/UserInfo';
 import { SideBar } from 'components/SideBar/SideBar';
 import { selectArrTasks } from 'redux/tasks/tasks-selectors';
 import { selectSideBar } from 'redux/modal/globalSelectors';
-import { openSideBar } from 'redux/modal/globalSlice';
+import { closeSideBar, openSideBar } from 'redux/modal/globalSlice';
 import goose from './goose.png';
 import icon from '../../images/icons.svg';
+import { useEffect } from 'react';
 
 export const Header = () => {
   const dispatch = useDispatch();
@@ -20,14 +21,14 @@ export const Header = () => {
   const isTabletOrMobile = useMedia('(max-width: 1439px)');
 
   const openMobileMenu = () => dispatch(openSideBar());
+  const closeMobileMenu = () => dispatch(closeSideBar());
   const sideBarState = useSelector(selectSideBar);
 
+  const handleOverlayClick = e => {if (e.target.classList.contains('overlay')) closeMobileMenu()};
+  
   // scroll lock, якщо Бургер активний + клас на Body в Global
-  if (sideBarState) {
-    document.body.classList.add('lock-scroll');
-  } else {
-    document.body.classList.remove('lock-scroll');
-  }
+  if (sideBarState) { document.body.classList.add('lock-scroll'); }
+  else {document.body.classList.remove('lock-scroll');}
 
   return (
     <Container>
@@ -35,7 +36,7 @@ export const Header = () => {
 // special mobile view
         <Wrapper>
           <Svg onClick={openMobileMenu}> <use href={icon + '#icon-menu'}></use> </Svg>
-          <Overlay isVisible={sideBarState}> {sideBarState && <SideBar />} </Overlay>          
+          <Overlay onClick={handleOverlayClick} isVisible={sideBarState} className="overlay"> {sideBarState && <SideBar />} </Overlay>          
           <Wrapper>
             <ThemeToggler />
             <UserInfo />
