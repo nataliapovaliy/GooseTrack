824abfebd16f48541b27228ac8be58a21f55b94d
Genokrad <zeeper90@gmail.com>
2023-04-26 00:14:57 +0300

    more validation and some css

--- src/components/Modal/Button/Button.styled.js
+++ src/components/Modal/Button/Button.styled.js
@@ -1,20 +1,26 @@
 import styled from 'styled-components';
 
 export const StyledButton = styled.button`
+  transition: all 0.3s;
+  cursor: pointer;
   border-radius: 8px;
   background-color: ${props => props.bcg};
   color: ${props => props.color};
   border: none;
-
   padding: 15px;
   display: flex;
   justify-content: center;
   width: 100%;
-
   font-family: 'Inter';
   font-weight: 600;
   font-size: 14px;
   line-height: 1.29;
+  &:hover {
+    border: 1px solid #42b8cc;
+  }
+  &:active {
+    filter: blur(0.1rem);
+  }
 `;
 
 export const ItemWraper = styled.div`
