b0f25a1245a4a0889d3f3ffde1b699d404f7f3d9
MariKa2525 <113701062+MariKa2525@users.noreply.github.com>
2023-04-25 11:33:32 +0300

    add

--- src/components/ChoosedMonth/CalendarTable/CalendarTable.jsx
+++ src/components/ChoosedMonth/CalendarTable/CalendarTable.jsx
@@ -13,27 +13,25 @@ import { useSelector } from 'react-redux';
 import { selectArrTasks } from 'redux/tasks/tasks-selectors';
 import { CalendarTaskDay } from './CalendarTaskDay/CalendarTaskDay';
 
-export const CalendarTable = ({ startDay, today})  => {
+export const CalendarTable = ({ startDay, today }) => {
   const tasks = useSelector(selectArrTasks);
 
-  const  totalDays = 42
-
-  // console.log(startDay.days())
-
-
+  const totalDays = 42;
 
   const day = startDay.clone().subtract(1, 'day');
   const daysArray = [...Array(totalDays)].map(() => day.add(1, 'day').clone());
   const isCurrentDay = day => moment().isSame(day, 'day');
   const isSelectedMonth = day => today.isSame(day, 'month');
-  
 
-  
+  // let options = { weekday: 'long'};
+
+  // const yyy = startDay.clone()
+
+  // console.log(new Intl.DateTimeFormat('en-US').format());
+  // // Monday
+
   // console.log("++++", tasks);
   // console.log('++++', tasks.length);
-
-
-
 
   let xxx = 0;
 
@@ -56,30 +54,27 @@ export const CalendarTable = ({ startDay, today})  => {
                 </DayWrapper>
               </ShowDayWrapper>
               <TaskListWrapper>
-                
-
-                  {tasks.map((task) => {
-                    if (task.createAt === dayItem.format('YYYY-MM-DD')) {
-                      xxx += 1;
-                      if (xxx <= 2) {
-                        return (
-                          <li key={task._id}>
-                            <CalendarTaskDay task={task} />
-                          </li>
-                        );
-                      } else if (xxx === 3) {
-                        return (
-                          <li key="more">
-                            <button>More...</button>
-                          </li>
-                        );
-                      } else if (xxx > 3) {
-                        xxx = 0;
-                      }
+                {tasks.map(task => {
+                  if (task.createAt === dayItem.format('YYYY-MM-DD')) {
+                    xxx += 1;
+                    if (xxx <= 2) {
+                      return (
+                        <li key={task._id}>
+                          <CalendarTaskDay task={task} />
+                        </li>
+                      );
+                    } else if (xxx === 3) {
+                      return (
+                        <li key="more">
+                          <button type='button'>More...</button>
+                        </li>
+                      );
+                    } else if (xxx > 3) {
+                      xxx = 0;
                     }
-                    return null;
-                  })}
-                
+                  }
+                  return null;
+                })}
               </TaskListWrapper>
             </RowInCell>
           </CellWrapper>
